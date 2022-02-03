aee476852f3c0ed5ceb5799ce2e8ca9f85c4cb5b
Marius <marius@example.com>
2022-02-03 16:53:09 +0200

    uzd

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.get('/api/people/:personId', (req, res) => {
   console.log('req ===', req.params.personId);
   const { personId } = req.params;
   // surasti people masyve objekta kurio id === 1
-  const found = people.find((person) => person.id === Number(personId));
+  const found = people.find((person) => person.sex === Number(personId));
   console.log('found ===', found);
   if (found) {
     // radom
@@ -52,6 +52,29 @@ app.get('/api/people/:personId', (req, res) => {
 });
 
 // GET /api/people/male (grazinam visus vyrus)
+// 1. sukuriam endpoint
+// const males = people.filter((person) => person.sex === 'male');
+// console.log('males ===', males);
+// 2. paieskom su filter ar kazkaip atrenkam vyrus
+// 3. grazinam vyru masyva
+
+// GET /api/people/gender/:sex
+// (jei sex === male atrenkam male)
+// (jei sex === female atrenkam female)
+// (jei sex === nei male nei female pranesam apie klaida su status 400)
+
+// GET /api/people/drivers (grazinam zmones turincius automobili)
+
+// GET /api/people/over30 (grazinam zmones kuriu amzius daugiau nei 30)
+
+// GET /api/people/over/:age (grazinam zmones kuriu amzius daugiau nei :age)
+
+// GET /api/people/income/avg (grazinam zmoniu atlyginimu vidurki)
+
+// GET /api/people/income/below/:salary (grazinam zmoniu masyva kurie uzdirba maziau nei :salary)
+//PVZ /api/people/income/below/1500
+
+// GET /api/people/married (grazinam vedusius zmones)
 
 // Run the server
 app.listen(PORT, console.log(`server online port: ${PORT}`));
